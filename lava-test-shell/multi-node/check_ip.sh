424dae3f9a833cc79bdf9bdad983c68c2c8a8204
Neil Williams <codehelp@debian.org>
2013-09-17 17:26:10 +0100

    Fix the check_ip support to fully test the group.

--- lava-test-shell/multi-node/check_ip.sh
+++ lava-test-shell/multi-node/check_ip.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
 set -e
-set -x
-GREP=""
-# if an argument is specified, use that to grep for this as a role
-if [ "$1" != "" ]; then
-	GREP="|grep $1"
-fi
-# lava-group is tab separated
-for line in `lava-group $GREP | cut -f1` ; do
+#set -x
+echo "Testing all members in the group"
+for line in `lava-group | sed -e 's/\(.*\)\s.*/\1/'` ; do
 	# get the ipv4 for this device
 	STR=`lava-network query $line ipv4`
 	# strip off the prefix for ipv4
-	DUT=`echo $STR | sed -e 's/^addr://'`
-	ping -c1 -W1 ${IP}
+	DUT=`echo $STR | sed -e 's/<LAVA.*>//g' | tr -d '\n' | sed -e 's/\s//g' | sed -e 's/^addr://'`
+	ping -c1 -W1 ${DUT}
 done
