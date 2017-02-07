1d85de07003f253d54a351c0535133047c0786fb
Senthil Kumaran S <senthil.kumaran@linaro.org>
2017-02-07 16:17:00 +0530

    Improve getting adb serial number script.
    Change-Id: I1caf8f24dbea7a699400a15baf9828a96eaa31d2

--- lava-test-shell/android/scripts/get-adb-serial.sh
+++ lava-test-shell/android/scripts/get-adb-serial.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-serial_no="$(adb get-serialno)"
-echo $serial_no
-
-if [ $serial_no = "unknown" ]; then
+which adb
+echo
+# start adb and stop the daemon start message from appearing in $result
+adb get-serialno || true # start daemon if not yet running.
+result=`adb get-serialno 2>&1 | tail -n1`
+if [ "$result" = "unknown" ]; then
+    echo "ERROR: adb get-serialno returned" $result
     exit 1
 else
+    echo "adb get-serialno returned" $result
+    echo $result > adb-connection.txt
     exit 0
 fi
