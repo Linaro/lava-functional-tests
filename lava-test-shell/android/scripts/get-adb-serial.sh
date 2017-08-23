2cd136882c0ecbf55474f1c4432f533c421e7355
Senthil Kumaran S <senthil.kumaran@linaro.org>
2017-08-23 22:03:43 +0530

    Modify get-adb-serial test definition to wait for adb device.
    Change-Id: Id51cec36390e9911b7fe73d9d5bba43a5ec9160d

--- lava-test-shell/android/scripts/get-adb-serial.sh
+++ lava-test-shell/android/scripts/get-adb-serial.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 which adb
+# start daemon if not yet running.
+adb start-server || true
+adb wait-for-device
 echo
 # start adb and stop the daemon start message from appearing in $result
-adb get-serialno || true # start daemon if not yet running.
+adb get-serialno || true
 result=`adb get-serialno 2>&1 | tail -n1`
 if [ "$result" = "unknown" ]; then
     echo "ERROR: adb get-serialno returned" $result
